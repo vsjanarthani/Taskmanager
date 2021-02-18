7853250390aa8fe10bd86fb88ba483d74c025fcf
jana <vs.janarthani@gmail.com>
2021-02-18 15:00:53 -0800

    modified jscode to finish add form

--- assets/script/taskmanager.js
+++ assets/script/taskmanager.js
@@ -1,13 +1,41 @@
 // Initialising DOM elements
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
+
+// Function to submit form
+formEl.addEventListener("submit", submitForm);
+function submitForm(event) {
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    if (!taskNameInput || !taskTypeInput) {
+        alert("Fill the form before adding!");
+        return false;
+      }
+    var formInput = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    addTask(formInput);
+    formEl.reset();
+}
+
 // Function to add new task
-buttonEl.addEventListener("click", addTask);
-function addTask() {
+function addTask(formInput) {
+    // create list item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
-    tasksToDoEl.appendChild(listItemEl);
-}
+
+    // create div to hold task info and add to list item
+  var taskInfoEl = document.createElement("div");
+  taskInfoEl.className = "task-info";
+  taskInfoEl.innerHTML = "<h3 class='task-name'>" + formInput.name + "</h3><span class='task-type'>" + formInput.type + "</span>";
+  listItemEl.appendChild(taskInfoEl);
+
+  // add entire list item to list
+  tasksToDoEl.appendChild(listItemEl);
+};
+
+
 
